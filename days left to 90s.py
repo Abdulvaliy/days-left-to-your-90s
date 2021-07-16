bb9eafb12ae7cff288823ac84f669ca22bb23e47
Abduvali Zokhidov <80103407+Abdulvaliy@users.noreply.github.com>
2021-07-17 00:17:35 +0500

    Add files via upload

---
+++ days left to 90s.py
@@ -0,0 +1,17 @@
+
+age = input("What is your current age? ")
+
+age= int(age)
+days = 365 * (90 - age)
+months = 12 * (90 - age)
+weeks = 52*(90 - age)
+years = 90 - age
+print(f"You have {years} years, {months} months, {weeks} weeks, {days} days left until you are 90 :) ")
+
+
+
+
+
+
+
+
